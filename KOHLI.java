04871477723c69994caf0b84f80af360130acc51
btshiva1718 <95751444+btshiva1718@users.noreply.github.com>
2023-01-04 14:32:06 +0530

    Update KOHLI.java

--- KOHLI.java
+++ KOHLI.java
@@ -1,5 +1,5 @@
 class KOHLI {
     public static void main(String[] args) {
-        System.out.println("Hello, World! i am bt"); 
+        System.out.println("Hello, World! i am btshiva"); 
     }
 }
